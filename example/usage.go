622083998ac4f97ff16ff63f9c1af0e24829625e
Paulo Borges <pborges475@gmail.com>
2025-03-07 18:42:54 -0800

    Reformat usage.go

--- example/usage.go
+++ example/usage.go
@@ -9,14 +9,57 @@ import (
 	"os"
 )
 
-type JSError struct {
-	Message  string
-	Location string
-}
+/**
+OUTPUT:
+* Normal print
+2025/03/07 18:40:06 usage.go:27: multiple Read calls return no data or error
 
-func (e JSError) Error() string {
-	return e.Message
-}
+* Detailed print
+2025/03/07 18:40:06 usage.go:30: multiple Read calls return no data or error
+│┬ usage.go:75 (main.a) ^
+│└┬ usage.go:79 (main.b) ^
+│ └┬ usage.go:83 (main.c) multiple Read calls return no data or error
+│  └┬ usage.go:87 (main.d) ^
+│   └─ usage.go:91 (main.e) EOF
+
+* Nil error
+2025/03/07 18:40:06 usage.go:33:
+
+* Wrapped Error
+2025/03/07 18:40:06 usage.go:36: i am no good
+│┬ usage.go:36 (main.main) i am no good
+│└─ usage.go:36 (main.main) EOF
+
+* Is io.EOF
+2025/03/07 18:40:06 usage.go:39: true
+
+* Is io.ErrNoProgress
+2025/03/07 18:40:06 usage.go:42: true
+
+* Is io.ErrClosedPipe
+2025/03/07 18:40:06 usage.go:45: false
+
+* Multiline
+2025/03/07 18:40:06 usage.go:48: multiline error
+│ line1
+│ line2
+│┬ usage.go:70 (main.multiline) ^
+│└┬ usage.go:69 (main.multiline) ^
+│ └┬ usage.go:68 (main.multiline) ^
+│  └─ usage.go:67 (main.multiline) multiline error
+│     line1
+│     line2
+
+* JSError with no transformer
+2025/03/07 18:40:06 usage.go:51: foo
+│┬ usage.go:51 (main.main) ^
+│└─ usage.go:51 (main.main) foo
+
+* JSError with with transformer
+2025/03/07 18:40:06 usage.go:63: foo => bar:12
+│┬ usage.go:63 (main.main) ^
+│└─ usage.go:63 (main.main) foo => bar:12
+**/
 
 func main() {
 	log.SetOutput(os.Stdout)
@@ -99,54 +142,11 @@ func noerr() error {
 	return errs.Push(nil)
 }
 
-/**
-OUTPUT:
-* Normal print
-2025/03/07 18:40:06 usage.go:27: multiple Read calls return no data or error
+type JSError struct {
+	Message  string
+	Location string
+}
 
-* Detailed print
-2025/03/07 18:40:06 usage.go:30: multiple Read calls return no data or error
-│┬ usage.go:75 (main.a) ^
-│└┬ usage.go:79 (main.b) ^
-│ └┬ usage.go:83 (main.c) multiple Read calls return no data or error
-│  └┬ usage.go:87 (main.d) ^
-│   └─ usage.go:91 (main.e) EOF
-
-* Nil error
-2025/03/07 18:40:06 usage.go:33:
-
-* Wrapped Error
-2025/03/07 18:40:06 usage.go:36: i am no good
-│┬ usage.go:36 (main.main) i am no good
-│└─ usage.go:36 (main.main) EOF
-
-* Is io.EOF
-2025/03/07 18:40:06 usage.go:39: true
-
-* Is io.ErrNoProgress
-2025/03/07 18:40:06 usage.go:42: true
-
-* Is io.ErrClosedPipe
-2025/03/07 18:40:06 usage.go:45: false
-
-* Multiline
-2025/03/07 18:40:06 usage.go:48: multiline error
-│ line1
-│ line2
-│┬ usage.go:70 (main.multiline) ^
-│└┬ usage.go:69 (main.multiline) ^
-│ └┬ usage.go:68 (main.multiline) ^
-│  └─ usage.go:67 (main.multiline) multiline error
-│     line1
-│     line2
-
-* JSError with no transformer
-2025/03/07 18:40:06 usage.go:51: foo
-│┬ usage.go:51 (main.main) ^
-│└─ usage.go:51 (main.main) foo
-
-* JSError with with transformer
-2025/03/07 18:40:06 usage.go:63: foo => bar:12
-│┬ usage.go:63 (main.main) ^
-│└─ usage.go:63 (main.main) foo => bar:12
-*/
+func (e JSError) Error() string {
+	return e.Message
+}
